845b425f0f512c1c4f869f517bd6d41a37a2e47c
Wakeel Kehinde Ige <tijjken@gmail.com>
2021-10-02 11:01:54 +0100

    files added

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,10 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBKgzufJxT_UgdyHFcDBn1xNQJ5_tU0_M8",
-  authDomain: "whatsapp-clone-bcd89.firebaseapp.com",
-  projectId: "whatsapp-clone-bcd89",
-  storageBucket: "whatsapp-clone-bcd89.appspot.com",
+  apiKey: "API_KEY",
+  authDomain: "PROJECT_ID.firebaseapp.com",
+  projectId: "PROJECT_ID",
+  storageBucket: "PROJECT_ID.appspot.com",
   messagingSenderId: "1077438697790",
   appId: "1:1077438697790:web:b684db3aa94c7d6de3dc6f",
   measurementId: "G-TP0T70CRLX",
